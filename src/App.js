2d3ebd34e9ff33e963367289e6d7da804eeda48e
Swapnil Sable <swapnil.sable@outlook.com>
2021-09-08 01:31:19 +0530

    Spotify config. Snip access token out of url.

--- src/App.js
+++ src/App.js
@@ -1,9 +1,31 @@
+import React, { useEffect, useState } from 'react';
 import './App.css';
+import Login from './Login';
+import { getTokenFromUrl } from './spotify';
 
 function App() {
+  const [token, setToken] = useState(null);
+
+  useEffect(() => {
+    const hash = getTokenFromUrl();
+    window.location.hash = "";
+    const _token = hash.access_token;
+
+    if (_token) {
+      setToken(_token)
+    }
+    console.log("Token ", token)
+  }, []);
+
   return (
     <div className="app">
-      <h1>Spotify Clone</h1>
+      {
+        token ? (
+          <h1>Logged in</h1>
+        ) : (
+          <Login />
+        )
+      }
     </div>
   );
 }
