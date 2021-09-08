79bb1c36d77f2e25316290445ce768c18d212446
Swapnil Sable <swapnil.sable@outlook.com>
2021-09-08 21:18:39 +0530

    Add and configure spotify-web-api wrapper.

--- src/App.js
+++ src/App.js
@@ -2,6 +2,10 @@ import React, { useEffect, useState } from 'react';
 import './App.css';
 import Login from './Login';
 import { getTokenFromUrl } from './spotify';
+import SpotifyWebApi from 'spotify-web-api-js';
+import Player from './Player';
+
+const spotify = new SpotifyWebApi();
 
 function App() {
   const [token, setToken] = useState(null);
@@ -13,6 +17,11 @@ function App() {
 
     if (_token) {
       setToken(_token)
+
+      spotify.setAccessToken(_token);
+      spotify.getMe().then(user => {
+        console.log(user);
+      })
     }
     console.log("Token ", token)
   }, []);
@@ -21,7 +30,7 @@ function App() {
     <div className="app">
       {
         token ? (
-          <h1>Logged in</h1>
+          <Player />
         ) : (
           <Login />
         )
