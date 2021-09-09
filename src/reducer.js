32712f6e753028a4758d84251932c7de68d73b2f
Swapnil Sable <swapnil.sable@outlook.com>
2021-09-10 01:57:12 +0530

    Build body header with avatar.

--- src/reducer.js
+++ src/reducer.js
@@ -1,6 +1,6 @@
 export const initialState = {
   user: null,
-  token: "BQBw6AWdyxZhUeKdGRzcO6PBpVViNfLMReb9zLtm9s1v6O_CNZELv7kcURIsHCPNmCdHZuDYCdClQ-s-OF4eEk5yP045FE2SgNB5ENsKmn5Bbo8JO8gjgv0as9KjfieghSGOpNXygKolPgpz_FavOwtY17EDmsTafTcbB1zkAV4wTs1AjCb57sE",
+  // token: "BQBw6AWdyxZhUeKdGRzcO6PBpVViNfLMReb9zLtm9s1v6O_CNZELv7kcURIsHCPNmCdHZuDYCdClQ-s-OF4eEk5yP045FE2SgNB5ENsKmn5Bbo8JO8gjgv0as9KjfieghSGOpNXygKolPgpz_FavOwtY17EDmsTafTcbB1zkAV4wTs1AjCb57sE",
   playlists: [],
   playing: false,
   item: null
